085e111256754531ed5baf20bf0425bce4dac5f4
M1hail228 <mishapetrov470@gmail.com@>
2023-12-07 19:44:39 +0300

    (Добавил информацию про input и решил задачу на сравнение с помощью count)

--- lesson1/Train.py
+++ lesson1/Train.py
@@ -5,9 +5,26 @@ print(x)
 # Работа с input
 x = int(input())
 
-if x < 5 or x > 10:
+if x == 52:
+    print('Писятдва')
+elif x < 5 or x > 10:
     print('Хороший выбор')
-
 else:
     print('Плохой выбор')
 
+# Объединение текста
+a = 'How are you '
+b = 52
+c = ' Да здравствует Санкт-Петербург и этот город наш'
+
+print(a + str(b) + c)
+
+
+# Дано трёхзначное число. Определить, есть ли среди его цифр одинаковые
+number = 455
+for compare in str(number):
+    if str(number).count(compare):
+        print('Есть одинаковые числа')
+        break
+    else:
+        print('Нет одинаковых чисел')
