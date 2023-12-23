6f0d3cd7ff55b99c6070100e58eb2082c016461a
M1hail228 <mishapetrov470@gmail.com@>
2023-12-23 15:51:11 +0300

    Закончил урок

--- lesson3/цикл for.py
+++ lesson3/цикл for.py
@@ -1,3 +1,6 @@
+from random import randint
+
+
 # # range(start, stop, step) - позволяет генерировать последовательности
 # range(5)  # 0,1,2,3,4
 # range(1, 5, 1)  # 1,2,3,4
@@ -69,18 +72,120 @@
 #         print(j)
 
 
+# print()
+#
+# print('Вывести сумму всех нечетных чисел от а до b (включая обе границы)')
+#
+# a2 = int(input())
+# b2 = int(input())
+# count = 0
+# print('Все числа в списке: ')
+#
+# for i in range(a2, b2 + 1):
+#     if i % 2 != 0:
+#         print(i)
+#         count += i
+# print('Ответ: ', count)
+
+
+# Методы списка:
+
+# len - длина списка
+# lst = [4, 6, 7]
+# print(len(lst))  # 3
+#
+# # Узнать последний эл-т с помощью len
+# print(lst[len(lst) - 1])
+#
+# # добавить объект в конец списка
+# lst.append(9)  # 4, 6, 7, 9
+# print(lst)
+#
+# # Удаление элемента. 2 способа
+# # del
+# del lst[0]  # Удалить 4
+#
+# # remove. Удалим 9
+# if 9 in lst:
+#     lst.remove(9)
+# else:
+#     print("Такого числа нет в списке")
+
+
+print('Есть список из 10 элементов, найти сумму элементов этого списка')
+lst = []
+for i in range(10):
+    lst.append(randint(-10, 10))
+print(lst)
+print('-----------------------------------')
+
+count = 0
+for i in lst:
+    count += i
+print(count)
 print()
 
-print('Вывести сумму всех нечетных чисел от а до b (включая обе границы)')
+count1 = 0
+print('Есть список из 10 элементов, посчитать сумму четных цифр в списке')
+for i in lst:
+    if i % 2 == 0:
+        count1 += i
+print(count1)
+print()
 
-a2 = int(input())
-b2 = int(input())
-count = 0
-print('Все числа в списке: ')
+print('Есть список из 10 элементов, найти наибольшее и наименьшее число в списке')
+max_num = lst[0]
+min_num = lst[0]
+for i in lst:
+    if i > max_num:
+        max_num = i
+    if i < min_num:
+        min_num = i
+print('Наибольшее число: ', max_num)
+print('Наименьшее число: ' + str(min_num))
+print()
 
-for i in range(a2, b2 + 1):
-    if i % 2 != 0:
-        print(i)
-        count += i
-print('Ответ: ', count)
+print('Прошлая задача, но нужно найти индекс минимального и максимального элемента в списке')
+index_max_nuber = 0
+index_min_nuber = 0
+s = 0
+
+for i in lst:
+    if i > max_num:
+        max_num = i
+        index_max_nuber = s
+    if i < min_num:
+        min_num = i
+        index_min_nuber = s
+    s += 1
+print('Наибольшее число: ', max_num, ' Его индекс = ', index_max_nuber)
+print('Наименьшее число: ' + str(min_num) + ' Его индекс = ' + str(index_min_nuber))
+print()
+
+# Второй способ
+for index in range(len(lst)):
+    if lst[index] > max_num:
+        max_num = lst[index]
+        index_max_nuber = index
+    if lst[index] < min_num:
+        min_nuber = lst[index]
+        index_min_nuber = index
+print('Наибольшее число: ', max_num, ' Его индекс = ', index_max_nuber)
+print('Наименьшее число: ' + str(min_num) + ' Его индекс = ' + str(index_min_nuber))
+print()
+
+
+# Третий способ
+i = 0
+while i < len(lst):
+    if lst[i] > max_num:
+        max_num = lst[i]
+        index_max_nuber = i
+    if lst[i] < min_num:
+        index_min_nuber = lst[i]
+        index_min_nuber = i
+    i += 1
+print('Наибольшее число: ', max_num, ' Его индекс = ', index_max_nuber)
+print('Наименьшее число: ' + str(min_num) + ' Его индекс = ' + str(index_min_nuber))
+print()
 
