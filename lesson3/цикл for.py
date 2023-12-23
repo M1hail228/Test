11d63bc75e409a5c9480815a1d977f66635bc7b2
M1hail228 <mishapetrov470@gmail.com@>
2023-12-23 17:12:43 +0300

    Создание списка с помощью одной строки

--- lesson3/цикл for.py
+++ lesson3/цикл for.py
@@ -112,80 +112,105 @@ from random import randint
 #     print("Такого числа нет в списке")
 
 
-print('Есть список из 10 элементов, найти сумму элементов этого списка')
-lst = []
+# print('Есть список из 10 элементов, найти сумму элементов этого списка')
+# lst = []
+# for i in range(10):
+#     lst.append(randint(-10, 10))
+# print(lst)
+# print('-----------------------------------')
+#
+# count = 0
+# for i in lst:
+#     count += i
+# print(count)
+# print()
+#
+# count1 = 0
+# print('Есть список из 10 элементов, посчитать сумму четных цифр в списке')
+# for i in lst:
+#     if i % 2 == 0:
+#         count1 += i
+# print(count1)
+# print()
+#
+# print('Есть список из 10 элементов, найти наибольшее и наименьшее число в списке')
+# max_num = lst[0]
+# min_num = lst[0]
+# for i in lst:
+#     if i > max_num:
+#         max_num = i
+#     if i < min_num:
+#         min_num = i
+# print('Наибольшее число: ', max_num)
+# print('Наименьшее число: ' + str(min_num))
+# print()
+#
+# print('Прошлая задача, но нужно найти индекс минимального и максимального элемента в списке')
+# index_max_nuber = 0
+# index_min_nuber = 0
+# s = 0
+#
+# for i in lst:
+#     if i > max_num:
+#         max_num = i
+#         index_max_nuber = s
+#     if i < min_num:
+#         min_num = i
+#         index_min_nuber = s
+#     s += 1
+# print('Наибольшее число: ', max_num, ' Его индекс = ', index_max_nuber)
+# print('Наименьшее число: ' + str(min_num) + ' Его индекс = ' + str(index_min_nuber))
+# print()
+#
+# # Второй способ
+# for index in range(len(lst)):
+#     if lst[index] > max_num:
+#         max_num = lst[index]
+#         index_max_nuber = index
+#     if lst[index] < min_num:
+#         min_nuber = lst[index]
+#         index_min_nuber = index
+# print('Наибольшее число: ', max_num, ' Его индекс = ', index_max_nuber)
+# print('Наименьшее число: ' + str(min_num) + ' Его индекс = ' + str(index_min_nuber))
+# print()
+#
+#
+# # Третий способ
+# i = 0
+# while i < len(lst):
+#     if lst[i] > max_num:
+#         max_num = lst[i]
+#         index_max_nuber = i
+#     if lst[i] < min_num:
+#         index_min_nuber = lst[i]
+#         index_min_nuber = i
+#     i += 1
+# print('Наибольшее число: ', max_num, ' Его индекс = ', index_max_nuber)
+# print('Наименьшее число: ' + str(min_num) + ' Его индекс = ' + str(index_min_nuber))
+# print()
+import random
+
+# Создание списков
+lst1 = []
 for i in range(10):
-    lst.append(randint(-10, 10))
+    lst1.append(random.randint(1, 10))
+print(lst1)
+
+'''
+Генерация списков
+синтакис:
+название_переменной = [формула for переменная in range(количество элементов)]
+'''
+
+lst = [random.randint(0,10) for i in range(10)]
 print(lst)
-print('-----------------------------------')
 
-count = 0
-for i in lst:
-    count += i
-print(count)
-print()
-
-count1 = 0
-print('Есть список из 10 элементов, посчитать сумму четных цифр в списке')
-for i in lst:
-    if i % 2 == 0:
-        count1 += i
-print(count1)
-print()
-
-print('Есть список из 10 элементов, найти наибольшее и наименьшее число в списке')
-max_num = lst[0]
-min_num = lst[0]
-for i in lst:
-    if i > max_num:
-        max_num = i
-    if i < min_num:
-        min_num = i
-print('Наибольшее число: ', max_num)
-print('Наименьшее число: ' + str(min_num))
-print()
-
-print('Прошлая задача, но нужно найти индекс минимального и максимального элемента в списке')
-index_max_nuber = 0
-index_min_nuber = 0
-s = 0
-
-for i in lst:
-    if i > max_num:
-        max_num = i
-        index_max_nuber = s
-    if i < min_num:
-        min_num = i
-        index_min_nuber = s
-    s += 1
-print('Наибольшее число: ', max_num, ' Его индекс = ', index_max_nuber)
-print('Наименьшее число: ' + str(min_num) + ' Его индекс = ' + str(index_min_nuber))
-print()
-
-# Второй способ
-for index in range(len(lst)):
-    if lst[index] > max_num:
-        max_num = lst[index]
-        index_max_nuber = index
-    if lst[index] < min_num:
-        min_nuber = lst[index]
-        index_min_nuber = index
-print('Наибольшее число: ', max_num, ' Его индекс = ', index_max_nuber)
-print('Наименьшее число: ' + str(min_num) + ' Его индекс = ' + str(index_min_nuber))
-print()
-
-
-# Третий способ
+print('Вывод списка через while ')
+#  Чтобы вывести список через while, нужно использовать len(количество элементов)
 i = 0
 while i < len(lst):
-    if lst[i] > max_num:
-        max_num = lst[i]
-        index_max_nuber = i
-    if lst[i] < min_num:
-        index_min_nuber = lst[i]
-        index_min_nuber = i
+    print(lst[i])
     i += 1
-print('Наибольшее число: ', max_num, ' Его индекс = ', index_max_nuber)
-print('Наименьшее число: ' + str(min_num) + ' Его индекс = ' + str(index_min_nuber))
+print(lst)
+print('Конец списка')
 print()
-
